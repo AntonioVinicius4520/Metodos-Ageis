3d1265348cf863dadfd5bd90aa97c39db3770187
alunos <catce.2022111mtds0266@aluno.ifpi.edu.br>
2022-10-27 15:08:01 -0300

    Criação do método calcular operação

--- op_B3.py
+++ op_B3.py
@@ -10,3 +10,13 @@ class OperacaoB3():
         self.valor_operacao_sem_taxa = self.valor_unitario * self.quantidade
         self.taxaB3 = self.valor_operacao_sem_taxa * (0.03 / 100)
         self.valor_operacao_com_taxa = self.calcular_operacao()
+
+    # Funcionando!
+    def calcular_operacao(self):
+        if self.tipo.lower() in ['c', 'compra']:
+            self.valor_operacao_com_taxa = round(self.valor_operacao_sem_taxa +
+                                                 self.corretagem + self.taxaB3, 2)
+        elif self.tipo.lower() in ['v', 'venda']:
+            self.valor_operacao_com_taxa = self.valor_operacao_sem_taxa - \
+                self.corretagem - self.taxaB3
+        return self.valor_operacao_com_taxa
